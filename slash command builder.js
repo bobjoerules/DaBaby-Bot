b77210ab683482a7e0fddcef0851d2cd0e9262d6
Bobjoerules <39201587+bobjoerules@users.noreply.github.com>
2022-11-10 23:05:43 -0800

    env hopefully

---
+++ slash command builder.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+console.log("NodeJS Version: " + process.version)
+const express = require('express');
+const app = express();
+
+
+const { Client, Intents, MessageAttachment } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const { MessageEmbed } = require('discord.js');
+client.once('ready', () => {
+    new SlashCommandBuilder()
+    .setName('Images')
+    .setDescription('Sends a random image of DaBaby'),
+});
+client.login(process.env.TOKEN)
